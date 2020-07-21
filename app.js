d631b158138466ad006ed86d8fc8ff2974d57cae
Abdiel Reyes <abykings1@hotmail.com>
2020-07-21 08:47:48 -0500

    se anadio manejo de errores

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ var lcd = new LCD(1, 0x27, COLS, ROWS);
 lcd.on();
 moment.locale('es');
 const NAME = 'Abdiel'
-const CITY = 'Ciudad de Mexico, MX'
+const CITY = 'Mexico City, MX'
+var prevDate;
+var prevHour;
+var prevSalutation;
+var prevWeather;
 
 function clearLine(line) {
     lcd.println(" ".repeat(COLS), line);
@@ -34,33 +38,31 @@ function getCurrentWeather() {
         weather.find({ search: CITY, degreeType: 'C' }, function (err, result) {
             if (err) {
                 resolve('Error al obtener clima')
-            };
-
+            }; 
+            console.log(result)
             var weather = {
                 currentTemp: result[0].current.temperature,
                 skyText: result[0].current.skytext
             }
-
             resolve(`Temp: ${weather.currentTemp}, ${weather.skyText}`)
         });
     })
 }
-var prevDate;
-var prevHour;
-var prevSalutation;
-var prevWeather;
+function lcdErrorHandler( err ) {
+    console.log( 'Unable to print to LCD display on bus 1 at address 0x27' );
+    //Disable further processing if application calls this recursively.
+};
 async function initScreen() {
     prevDate = getDate()
     prevHour = getHour()
     prevSalutation = getSalutation()
     prevWeather = await getCurrentWeather()
-    lcd.clear();
-    lcd.println(prevSalutation, 1);
-    lcd.println(prevDate, 2);
-    lcd.println(prevHour, 3);
-    lcd.println(prevWeather, 4);
+    lcd.clear(); if( lcd.error )lcdErrorHandler( lcd.error );
+    lcd.println(prevSalutation, 1);if( lcd.error )lcdErrorHandler( lcd.error );
+    lcd.println(prevDate, 2);if( lcd.error )lcdErrorHandler( lcd.error );
+    lcd.println(prevHour, 3);if( lcd.error )lcdErrorHandler( lcd.error );
+    lcd.println(prevWeather, 4);if( lcd.error )lcdErrorHandler( lcd.error );
 }
-initScreen();
 
 function printClock() {
     var actualSalutation = getSalutation();
@@ -68,33 +70,32 @@ function printClock() {
     var actualHour = getHour();
     if (prevSalutation != actualSalutation) {
         clearLine(1);
-        lcd.println(actualSalutation, 1);
+        lcd.println(actualSalutation, 1);if( lcd.error )lcdErrorHandler( lcd.error );
         prevSalutation = actualSalutation;
     }
     if (prevDate != actualDate) {
         clearLine(2);
-        lcd.println(actualDate, 2);
+        lcd.println(actualDate, 2);if( lcd.error )lcdErrorHandler( lcd.error );
         prevDate = actualDate;
     }
     if (prevHour != actualHour) {
         clearLine(3);
-        lcd.println(actualHour, 3);
+        lcd.println(actualHour, 3);if( lcd.error )lcdErrorHandler( lcd.error );
         prevHour = actualHour;
     }
 }
 async function printWeather() {
-    var actualWeather = await getCurrentWeather();
-    console.log(actualWeather);
+    var actualWeather = await getCurrentWeather()
     if (prevWeather != actualWeather) {
         clearLine(4)
-        lcd.println(actualWeather, 4);
+        lcd.println(actualWeather, 4);if( lcd.error )lcdErrorHandler( lcd.error );
         prevWeather = actualWeather;
     }
 }
-
+initScreen();
 setInterval(() => {
     printClock();
 }, 1000);
 setInterval(() => {
     printWeather();
-}, 5000);
+}, 10000);
