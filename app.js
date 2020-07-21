d93504efb1633a80b5dfba7309ce50664683fd7d
Abdiel Reyes <abykings1@hotmail.com>
2020-07-20 20:22:49 -0500

    finished clock

--- app.js
+++ app.js
@@ -1,3 +1,44 @@
 var LCD = require('lcdi2c');
 var moment = require('moment');
-moment().format(); 
+var lcd = new LCD(1, 0x27, 20, 4);
+
+lcd.off();
+moment.locale('es');
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+function getSalutation() {
+    const currentHour = this.Moment().format("HH");
+    if (currentHour == 0 || currentHour < 12) return "Buenos dias!"
+    else if (currentHour <= 19) return "Buenas tardes!"
+    else return "Buenas noches!"
+}
+function getDate(){
+    return capitalizeFirstLetter(moment(Date.now()).format('dddd, DD MMMM yyyy'));
+}
+function getHour(){
+    return moment(Date.now()).format('h:mm a');
+}
+var prevDate=getDate()
+var prevHour=getHour()
+var prevSalutation=getSalutation()
+function initScreen() {
+    lcd.clear();
+    lcd.println(getSalutation(), 1);
+    lcd.println(fecha, 2);
+    lcd.println(hora, 3);
+    lcd.println('                    ', 4);
+}
+initScreen();
+setInterval(() => {
+    if(prevSalutation != getSalutation()){
+        lcd.println(getSalutation(), 1);
+    }
+    if(prevDate != getDate()){
+        lcd.println(getDate(), 2);
+    }
+    if(prevHour != getHour()){
+        lcd.println(getHour(), 3);
+    }
+}, 1000);
