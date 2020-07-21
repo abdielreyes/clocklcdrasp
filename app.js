728cd26f4ed393acd5ed441bdc8f5a64e6f4fd5c
Abdiel Reyes <abykings1@hotmail.com>
2020-07-20 23:05:54 -0500

    corregido limpiar linea

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var LCD = require('lcdi2c');
 var moment = require('moment');
 var weather = require('weather-js');
-
-var lcd = new LCD(1, 0x27, 20, 4);
+const COLS = 20;
+const ROWS = 4;
+var lcd = new LCD(1, 0x27, COLS, ROWS);
 
 lcd.on();
 moment.locale('es');
 const NAME = 'Abdiel'
 const CITY = 'Ciudad de Mexico, MX'
+
+function clearLine(line) {
+    lcd.println(" ".repeat(COLS), line);
+}
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -19,23 +24,23 @@ function getSalutation() {
     else return `Buenas noches ${NAME}!`
 }
 function getDate() {
-    return capitalizeFirstLetter(moment(Date.now()).format('dddd DD/MM/YYYY'));
+    return capitalizeFirstLetter(moment(Date.now()).format('dddd, DD-MM-YYYY'));
 }
 function getHour() {
     return moment(Date.now()).format('h:mm a');
 }
 function getCurrentWeather() {
-    return new Promise((resolve,reject)=>{
-        weather.find({ search: CITY, degreeType: 'C' }, function (err, result) {  
+    return new Promise((resolve, reject) => {
+        weather.find({ search: CITY, degreeType: 'C' }, function (err, result) {
             if (err) {
                 resolve('Error al obtener clima')
             };
-            
+
             var weather = {
                 currentTemp: result[0].current.temperature,
                 skyText: result[0].current.skytext
             }
-            
+
             resolve(`Temp: ${weather.currentTemp}, ${weather.skyText}`)
         });
     })
@@ -57,31 +62,36 @@ async function initScreen() {
 }
 initScreen();
 
-function printClock(){
+function printClock() {
     var actualSalutation = getSalutation();
     var actualDate = getDate();
     var actualHour = getHour();
     if (prevSalutation != actualSalutation) {
+        clearLine(1);
         lcd.println(actualSalutation, 1);
         prevSalutation = actualSalutation;
     }
     if (prevDate != actualDate) {
+        clearLine(2);
         lcd.println(actualDate, 2);
         prevDate = actualDate;
     }
     if (prevHour != actualHour) {
+        clearLine(3);
         lcd.println(actualHour, 3);
         prevHour = actualHour;
     }
 }
-async function printWeather(){
+async function printWeather() {
     var actualWeather = await getCurrentWeather();
-    
+    console.log(actualWeather);
     if (prevWeather != actualWeather) {
+        clearLine(4)
         lcd.println(actualWeather, 4);
         prevWeather = actualWeather;
     }
 }
+
 setInterval(() => {
     printClock();
 }, 1000);
